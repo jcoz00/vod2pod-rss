99e24b2061254d5b061e6356453b726644b0fd07
James Cozzi <jamescozzi00@gmail.com>
2026-03-01 19:25:40 -0600

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,21 @@ use simple_logger::SimpleLogger;
 use std::{env, net::TcpListener, process::exit};
 use vod2pod_rss::server;
 
+fn install_rustls_provider() {
+    // Choose ONE provider (aws-lc-rs is a good default)
+    rustls::crypto::aws_lc_rs::default_provider()
+        .install_default()
+        .expect("install rustls crypto provider");
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // Must be done early (before any TLS usage)
+    install_rustls_provider();
+
     let args: Vec<String> = env::args().collect();
-    if let Some("--version") = args.get(1).cloned().as_deref() {
-        let app_version = env!("CARGO_PKG_VERSION");
-        println!("{}", app_version);
+    if args.get(1).map(String::as_str) == Some("--version") {
+        println!("{}", env!("CARGO_PKG_VERSION"));
         exit(0);
     }
 
@@ -19,17 +28,16 @@ async fn main() -> std::io::Result<()> {
         .unwrap();
 
     if let Err(err) = flush_redis_on_new_version().await {
-        panic!(
-            "Error interacting with Redis (redis is required): {:?}",
-            err
-        );
+        panic!("Error interacting with Redis (redis is required): {:?}", err);
     }
 
     let listener = TcpListener::bind("0.0.0.0:8080").expect("Failed to bind");
     info!("listening on http://{}", listener.local_addr().unwrap());
+
     server::spawn_server(listener)
         .expect("could not setup server")
         .await?;
+
     Ok(())
 }
 
@@ -48,11 +56,11 @@ async fn flush_redis_on_new_version() -> eyre::Result<()> {
     if let Some(ref cached_version) = cached_version {
         if cached_version != app_version {
             info!("detected version change ({cached_version} != {app_version}) flushing redis DB");
-            () = redis::cmd("FLUSHDB").query_async(&mut con).await?;
+            redis::cmd("FLUSHDB").query_async(&mut con).await?;
         }
     }
 
-    () = redis::cmd("SET")
+    redis::cmd("SET")
         .arg("version")
         .arg(app_version)
         .query_async(&mut con)
@@ -61,4 +69,3 @@ async fn flush_redis_on_new_version() -> eyre::Result<()> {
 
     Ok(())
 }
-
